643d86790d4dceeeaac4bf20b5407097e519d994
Nischal Sharma <nischal@web3labs.com>
2024-06-02 18:42:07 +0530

    added scripts and tests
    Signed-off-by: Nischal Sharma <nischal@web3labs.com>

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -2,12 +2,21 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external {
-        vm.startBroadcast();
+    function deployFundMe() public returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig.activeNetworkConfig();
 
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
+        return (fundMe, helperConfig);
+    }
+
+    function run() external returns (FundMe, HelperConfig) {
+        return deployFundMe();
     }
 }
